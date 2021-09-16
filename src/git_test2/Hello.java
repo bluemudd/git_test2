398cbe6a8a117f9ebc996de745de75312a16ac9c
Park <parkjm0817@gmail.com>
2021-09-16 18:53:55 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,9 +2,9 @@ package git_test2;
 
 public class Hello {
 
-    public void say() {
 	String message = "안녕하세요";
-        System.out.println(message);
+       public String say(){
+	return message;
     }
 
 }
